5db48b3920d3322847a7529ba2bcc8e251c93ba6
Djhonattan <32268660+Djhonattan@users.noreply.github.com>
2017-10-24 15:16:27 -0200

    Add files via upload

---
+++ desenvolv/script.js
@@ -0,0 +1,24 @@
+
+$(document).ready(function(){
+	//$("#soma").click 
+	/*$("#soma").click(function(){
+		var primeiro, segundo;
+	 primeiro = $("#primeiro").val();
+	 segundo = $("#segundo").val();
+	 $("#resultado").text(parseInt(primeiro)+parseInt(segundo));
+	 //alert(parseInt(primeiro)+parseInt(segundo));*/
+
+
+	$('.amarelo').click(function(){
+	$('.azul').toggleClass('escondido');
+	})
+
+})
+
+/*
+$(document).ready(function)(){
+	$(".nome").click(function(){
+		$(".detalhes").toggleClass("escondido");
+	})
+})
+*/
